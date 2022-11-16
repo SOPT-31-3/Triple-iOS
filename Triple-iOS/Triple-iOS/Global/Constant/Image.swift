13c9dd728abfe5716d1053b5d5a2104fddba9bc5
cchanmi <gkfn980@hs.ac.kr>
2022-11-17 03:17:07 +0900

    [Chore] #3 - 빈 디렉토리에 파일 추가

---
+++ Triple-iOS/Triple-iOS/Global/Constant/Image.swift
@@ -0,0 +1,15 @@
+//
+//  Image.swift
+//  Triple-iOS
+//
+//  Created by 황찬미 on 2022/11/17.
+//
+
+import UIKit
+
+extension Constant {
+    struct Image {
+        // 상수화 해서 사용
+        static let icX = UIImage(named: "icX")
+    }
+}
